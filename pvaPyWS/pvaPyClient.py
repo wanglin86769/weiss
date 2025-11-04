590ebe5ed509a50eb0011c83be2e6fdd7ef01fa5
AndreFavotto <andrefavotto@hotmail.com>
2025-11-04 21:42:17 +0100

    pvaPyClient: simplify put procedure

--- pvaPyWS/pvaPyClient.py
+++ pvaPyWS/pvaPyClient.py
@@ -25,37 +25,6 @@ class PvaPyClient:
       self._handle_update(pv_name, value)
     return callback
   
-  def _on_write(self, pv):
-    """Internal callback for pv write handling."""
-    # do nothing for now
-    return
-
-  def _on_write_error(self, code):
-    """Internal callback for pv write error handling."""
-    print('Write failed. Returned error code: %s' % code)
-    
-  def _make_pvobject(self, ch, value):
-    field = ch.get().getStructureDict()
-    # Simple scalar types
-    if 'value' in field:
-        vfield = field['value']
-        # Enum: dict with 'index' and 'choices'
-        if isinstance(vfield, dict) and 'index' in vfield and 'choices' in vfield:
-            val = ch.get()["value"]
-            choices = [str(c) for c in val['choices']]
-            return NtEnum(choices, int(value))
-        # Scalar INT
-        elif vfield == ScalarType.INT:
-            return PvInt(int(value))
-        # Scalar DOUBLE
-        elif vfield == ScalarType.DOUBLE:
-            return PvDouble(float(value))
-        # Scalar STRING
-        elif vfield == ScalarType.STRING:
-            return PvString(str(value))
-    # fallback
-    return PvString(str(value))
-  
   def subscribe(self, client_id: str, pv_name: str):
     """Subscribe a single client to a PV."""
     if pv_name not in self._channels:
@@ -109,5 +78,7 @@ class PvaPyClient:
     if not ch:
       print(f"Trying to write to not subscribed {pv}. Ignoring.")
       return
-    obj = self._make_pvobject(ch, value)
-    ch.asyncPut(obj, self._on_write, self._on_write_error)
+    try:
+      ch.put(value)
+    except Exception as e:
+      print(f"Error writing to PV {pv}: {e}")
