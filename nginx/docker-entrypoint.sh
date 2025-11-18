8eeb95ffd5a477cade04e43b00ac16ddc74f5677
AndreFavotto <andrefavotto@hotmail.com>
2025-11-18 19:02:01 +0100

    load server name dynamically

--- nginx/docker-entrypoint.sh
+++ nginx/docker-entrypoint.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
-envsubst '$PROD_PORT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
+
+if [ -z "$PROD_PORT" ]; then
+  echo "ERROR: PROD_PORT must be set. Check your .env"
+  exit 1
+fi
+
+: "${SERVER_NAME:=localhost}"
+
+envsubst '$PROD_PORT $SERVER_NAME' \
+  < /etc/nginx/conf.d/default.conf.template \
+  > /etc/nginx/conf.d/default.conf
+
 exec nginx -g 'daemon off;'
